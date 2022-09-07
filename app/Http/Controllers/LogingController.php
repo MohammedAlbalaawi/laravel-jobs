70b2f5b243a272fba10dab50bc37a0f3aeefae2c
Mohammed Albalaawi <m.cip@live.com>
2022-09-07 18:49:42 +0300

    ADD login form request validation

--- app/Http/Controllers/LogingController.php
+++ app/Http/Controllers/LogingController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 
 class LogingController extends Controller
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -24,23 +24,32 @@
         <div class="card-body login-card-body">
             <p class="login-box-msg">Sign in to start your session</p>
 
-            <form action="../../index3.html" method="post">
+            <form action="{{route('login.check')}}" method="post">
+                @csrf
+                @error('email')
+                <span class="text-red">{{ $message }}</span>
+                @enderror
                 <div class="input-group mb-3">
-                    <input type="email" class="form-control" placeholder="Email">
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-envelope"></span>
                         </div>
                     </div>
                 </div>
+
+                @error('password')
+                <span class="text-red">{{ $message }}</span>
+                @enderror
                 <div class="input-group mb-3">
-                    <input type="password" class="form-control" placeholder="Password">
+                    <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-lock"></span>
                         </div>
                     </div>
                 </div>
+
                 <div class="row">
                     <div class="col-8">
                         <div class="check-primary">
