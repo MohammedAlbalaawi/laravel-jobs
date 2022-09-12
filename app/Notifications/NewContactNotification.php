6c6d2c1b098d5fe2cafd190038e394f894463120
Mohd Albalaawi <m.cip@live.com>
2022-09-12 14:21:55 +0300

    Mail Notification
    Send mail notification via MailTrap after someone contact us from our Contact form

---
+++ app/Notifications/NewContactNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Contact;
+
+class NewContactNotification extends Notification
+{
+    use Queueable;
+
+    protected $contact;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                ->greeting('Hello, ' . $notifiable->name)
+                ->line('This is a test message')
+                ->action('Show in dashboard', route('contacts.index'))
+                ->line('Thank you');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
